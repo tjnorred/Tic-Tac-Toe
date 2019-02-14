25ca4d2581cfe9d1fa48a9af7c30e2132ef0dd1d
TJ <tjnorred@gmail.com>
2019-02-14 18:28:24 -0500

    Create a tictactoe app and publish to github pages

---
+++ src/Board.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      switchTurns: true,
+      availableMoves: 9,
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    let moves = this.state.availableMoves - 1
+    if(calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.switchTurns ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      switchTurns: !this.state.switchTurns,
+      availableMoves: moves,
+    })
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if(winner) {
+      status = winner + ' wins!'
+    } else if(this.state.availableMoves === 0) {
+      status = 'Tie game!'
+    } else {
+      status = 'Next player: ' + (this.state.switchTurns ? 'X' : 'O')
+    }
+
+    return (
+      <div>
+        <div className="title">Tic Tac Toe</div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        <div className="start-button">
+        Start Game
+        </div>
+      </div>
+    )
+  }
+}
+
+function calculateWinner(squares) {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < winningCombos.length; i++) {
+    const [a, b, c] = winningCombos[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+export default Board
