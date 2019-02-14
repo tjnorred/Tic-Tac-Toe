45097b3c47bc86d9e130232f9003312e9c23a396
TJ <tjnorred@gmail.com>
2019-02-14 18:26:42 -0500

    Updates

---
+++ precache-manifest.be4889ad60704640980e6d39423f426d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72d2a57eca9de8d97851",
+    "url": "/tic-tac-toe/static/js/runtime~main.72d2a57e.js"
+  },
+  {
+    "revision": "1d7f8dfb891f0f9f7738",
+    "url": "/tic-tac-toe/static/js/main.1d7f8dfb.chunk.js"
+  },
+  {
+    "revision": "0e166512a363bc9a5e6e",
+    "url": "/tic-tac-toe/static/js/2.0e166512.chunk.js"
+  },
+  {
+    "revision": "1d7f8dfb891f0f9f7738",
+    "url": "/tic-tac-toe/static/css/main.6ba1e589.chunk.css"
+  },
+  {
+    "revision": "c97fe8dbe506569e3f1285ca865eb670",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
